4dd94000145a9fd20f54799bc89199ba2a6d8ee2
Evgeny Agafonov <js-coder@mail.ru>
2016-11-20 07:23:07 +0300

    format code

--- query.js
+++ query.js
@@ -10,43 +10,43 @@
 
 function generateQueries(keywords, maxCharLength, delimiter){
 
-	let queries = [];
-	let query = '';
-	
-	// avoid to modify source keywords array
-	keywords = keywords.slice();
-	
-	keywords.sort((k1, k2) => k1.length < k2.length ? 1 : k1.length > k2.length ? -1 : 0)
-	
-	while(keywords.length && keywords[0].length > maxCharLength) 
-		keywords.shift();
-	
-	while(keywords.length && keywords[0].length >= maxCharLength - delimiter.length) 
-		queries.push(keywords.shift());
-	
-	while(keywords.length){
+    let queries = [];
+    let query = '';
+    
+    // avoid to modify source keywords array
+    keywords = keywords.slice();
+    
+    keywords.sort((k1, k2) => k1.length < k2.length ? 1 : k1.length > k2.length ? -1 : 0)
+    
+    while(keywords.length && keywords[0].length > maxCharLength) 
+        keywords.shift();
+    
+    while(keywords.length && keywords[0].length >= maxCharLength - delimiter.length) 
+        queries.push(keywords.shift());
+    
+    while(keywords.length){
 
-		let index = 0;
-		query = query || keywords.shift();
-		
-		while(index < keywords.length && query.length + delimiter.length + keywords[index].length > maxCharLength) 
-			index++;
-		
-		if(index === keywords.length) {
-			queries.push(query);
-			query = '';
-		} else {
-			query = keywords
-					.splice(index,1)
-					.concat(query)
-					.join(delimiter);
-		}
-		
-	}
+        let index = 0;
+        query = query || keywords.shift();
+        
+        while(index < keywords.length && query.length + delimiter.length + keywords[index].length > maxCharLength) 
+            index++;
+        
+        if(index === keywords.length) {
+            queries.push(query);
+            query = '';
+        } else {
+            query = keywords
+                    .splice(index,1)
+                    .concat(query)
+                    .join(delimiter);
+        }
+        
+    }
 
-	if(!queries.length && query) queries.push(query);
-	
-	return queries;
+    if(!queries.length && query) queries.push(query);
+    
+    return queries;
 }
 
 module.exports = generateQueries;
